2416552abafc85740a270fd26fe3c64ba9fe3c12
simran <simrangurdasani05@gmail.com>
2023-12-26 16:25:29 +0530

    lec 4 new concept prime no checker

--- lecture4/script.js
+++ lecture4/script.js
@@ -1,94 +1,219 @@
-let products = [
+// let products = [
     
-        {
-          "name" : "baked beans",
-          "price" : 0.40,
-          "image" : "beans.jpg",
-          "type" : "vegetables"
-        },
-        {
-          "name" : "hot dogs",
-          "price" : 1.99,
-          "image" : "hotdogs.jpg",
-          "type" : "meat"
-        },
-        {
-          "name" : "spam",
-          "price" : 2.85,
-          "image" : "spam.jpg",
-          "type" : "meat"
-        },
-        {
-          "name" : "refried beans",
-          "price" : 0.99,
-          "image" : "refried.jpg",
-          "type" : "vegetables"
-        },
-        {
-          "name" : "kidney beans",
-          "price" : 0.58,
-          "image" : "kidney.jpg",
-          "type" : "vegetables"
-        },
-        {
-          "name" : "garden peas",
-          "price" : 0.52,
-          "image" : "gardenpeas.jpg",
-          "type" : "vegetables"
-        },
-        {
-          "name" : "mushy peas",
-          "price" : 0.58,
-          "image" : "mushypeas.jpg",
-          "type" : "vegetables"
-        },
-        {
-          "name" : "corned beef",
-          "price" : 2.39,
-          "image" : "cornedbeef.jpg",
-          "type" : "meat"
-        },
-        {
-          "name" : "tomato soup",
-          "price" : 1.40,
-          "image" : "tomatosoup.jpg",
-          "type" : "soup"
-        },
-        {
-          "name" : "chopped tomatoes",
-          "price" : 0.45,
-          "image" : "tomato.jpg",
-          "type" : "vegetables"
-        },
-        {
-          "name" : "chicken noodle soup",
-          "price" : 1.89,
-          "image" : "chickennoodle.jpg",
-          "type" : "soup"
-        },
-        {
-          "name" : "carrot and coriander soup",
-          "price" : 1.49,
-          "image" : "carrotcoriander.jpg",
-          "type" : "soup"
-        }
+//         {
+//           "name" : "baked beans",
+//           "price" : 0.40,
+//           "image" : "beans.jpg",
+//           "type" : "vegetables"
+//         },
+//         {
+//           "name" : "hot dogs",
+//           "price" : 1.99,
+//           "image" : "hotdogs.jpg",
+//           "type" : "meat"
+//         },
+//         {
+//           "name" : "spam",
+//           "price" : 2.85,
+//           "image" : "spam.jpg",
+//           "type" : "meat"
+//         },
+//         {
+//           "name" : "refried beans",
+//           "price" : 0.99,
+//           "image" : "refried.jpg",
+//           "type" : "vegetables"
+//         },
+//         {
+//           "name" : "kidney beans",
+//           "price" : 0.58,
+//           "image" : "kidney.jpg",
+//           "type" : "vegetables"
+//         },
+//         {
+//           "name" : "garden peas",
+//           "price" : 0.52,
+//           "image" : "gardenpeas.jpg",
+//           "type" : "vegetables"
+//         },
+//         {
+//           "name" : "mushy peas",
+//           "price" : 0.58,
+//           "image" : "mushypeas.jpg",
+//           "type" : "vegetables"
+//         },
+//         {
+//           "name" : "corned beef",
+//           "price" : 2.39,
+//           "image" : "cornedbeef.jpg",
+//           "type" : "meat"
+//         },
+//         {
+//           "name" : "tomato soup",
+//           "price" : 1.40,
+//           "image" : "tomatosoup.jpg",
+//           "type" : "soup"
+//         },
+//         {
+//           "name" : "chopped tomatoes",
+//           "price" : 0.45,
+//           "image" : "tomato.jpg",
+//           "type" : "vegetables"
+//         },
+//         {
+//           "name" : "chicken noodle soup",
+//           "price" : 1.89,
+//           "image" : "chickennoodle.jpg",
+//           "type" : "soup"
+//         },
+//         {
+//           "name" : "carrot and coriander soup",
+//           "price" : 1.49,
+//           "image" : "carrotcoriander.jpg",
+//           "type" : "soup"
+//         }
       
       
-]
+// ]
 
 // implementing search feature
 // type-bak
 // display all products whose "name" includes "bak"
 // includes => str.includes("bak") => return true if exists else return false
 
-let searchString = prompt("Enter product to be searched")
-products.forEach((value)=>{
-    if(value.name.includes(searchString)){
-    console.log(value.name)
-    }
-}
-)
+// let searchString = prompt("Enter product to be searched")
+// products.forEach((value)=>{
+//     if(value.name.includes(searchString)){
+//     console.log(value.name)
+//     }
+// }
+// )
 
 // filtered function se saerch ye filter function return true or false krta h
-let filteredProducts = products.filter((value) => value.name.includes(searchString))
-console.log(filteredProducts)
+// let filteredProducts = products.filter((value) => value.name.includes(searchString))
+// console.log(filteredProducts)
+
+// const header = document.getElementById('header')
+// console.log(header)
+
+// const para = document.getElementsByClassName('para')
+// console.log(para)
+
+const headerByQuery = document.querySelector('#header')
+console.log(headerByQuery)
+
+// agar meko div ke andar wala p select krna ho toh querySelector use hota h
+const para = document.querySelector('.para')
+console.log(para)
+
+setTimeout(()=>{
+  // headerByQuery.className = 'red'
+  // para.id='p1'
+
+  // new way of writing className and id by writing setAttribute(jo class h, aur jo krna h)
+  headerByQuery.setAttribute('class','red')
+  para.setAttribute('id','p1')
+},3000)
+
+let content = para.textContent
+console.log(content)
+
+let products = [
+    
+  {
+    "name" : "baked beans",
+    "price" : 0.40,
+    "image" : "beans.jpg",
+    "type" : "vegetables"
+  },
+  {
+    "name" : "hot dogs",
+    "price" : 1.99,
+    "image" : "hotdogs.jpg",
+    "type" : "meat"
+  },
+  {
+    "name" : "spam",
+    "price" : 2.85,
+    "image" : "spam.jpg",
+    "type" : "meat"
+  },
+  {
+    "name" : "refried beans",
+    "price" : 0.99,
+    "image" : "refried.jpg",
+    "type" : "vegetables"
+  },
+  {
+    "name" : "kidney beans",
+    "price" : 0.58,
+    "image" : "kidney.jpg",
+    "type" : "vegetables"
+  },
+  {
+    "name" : "garden peas",
+    "price" : 0.52,
+    "image" : "gardenpeas.jpg",
+    "type" : "vegetables"
+  },
+  {
+    "name" : "mushy peas",
+    "price" : 0.58,
+    "image" : "mushypeas.jpg",
+    "type" : "vegetables"
+  },
+  {
+    "name" : "corned beef",
+    "price" : 2.39,
+    "image" : "cornedbeef.jpg",
+    "type" : "meat"
+  },
+  {
+    "name" : "tomato soup",
+    "price" : 1.40,
+    "image" : "tomatosoup.jpg",
+    "type" : "soup"
+  },
+  {
+    "name" : "chopped tomatoes",
+    "price" : 0.45,
+    "image" : "tomato.jpg",
+    "type" : "vegetables"
+  },
+  {
+    "name" : "chicken noodle soup",
+    "price" : 1.89,
+    "image" : "chickennoodle.jpg",
+    "type" : "soup"
+  },
+  {
+    "name" : "carrot and coriander soup",
+    "price" : 1.49,
+    "image" : "carrotcoriander.jpg",
+    "type" : "soup"
+  }
+
+
+]
+
+
+// ye fir 1st index ka pura obj print krega product ka
+console.log(products[content])
+
+let dynamicELem = document.querySelector('.dynamic')
+console.log(dynamicELem)
+// works same
+dynamicELem.textContent = "hello"
+dynamicELem.innerHTML = "hello"
+
+// h1 as it is print krega
+// dynamicELem.textContent = "<h1>hello</h1>"
+// parse it and render
+// dynamicELem.innerHTML = "<h1>hello</h1>"
+// innerHTML is insecure kyuki kuch b user html m likhke screen par laa skta h
+let v = prompt("enter")
+console.log(v)
+dynamicELem.textContent = v
+
+
+
---
+++ lecture4/second.js
@@ -0,0 +1,52 @@
+// let random = parseInt(Math.random()*10)
+// console.log(random)
+
+// let res = document.querySelector('.result')
+// if(random<5){
+//     res.textContent="you lose"+random
+// }
+// else{
+//     res.textContent="you win"+random
+// }
+
+
+// // template strings - backticks
+// console.log(`Random is ${random}`)
+
+function check(){
+    let ans=document.querySelector('#answer')
+    let element = document.querySelector('#ip')
+    // agar input se kuch print karwana ho toh element.value likhte h
+    // console.log(element.value)
+    if(isNaN(parseInt(element.value))){
+        ans.textContent = "enter a valid no!!"
+    element.focus()
+
+        return
+    }
+    // ham aese likh skte, let value = element.value 
+    // for(let num=2;num<element.value;num++){
+               let flag=true;
+                for(let i=2;i<element.value;i++){
+                    if(element.value%i==0){
+                        flag=false;
+                        break;
+                    }
+                }
+                
+                if(flag){
+                    // console.log("is prime");
+                    ans.textContent = `${element.value} is prime`
+                }
+                else{
+                    ans.textContent = `${element.value} is not prime`
+                }
+                element.focus();
+                element.value='';
+         }
+        
+
+
+
+
+
